3a76d7273b83ec79a5125840addb8ee2d31f0789
Luis Silveira <luisilveira@gmail.com>
2020-05-01 01:43:29 +0100

    fix repeatable schedule

--- src/main/kotlin/com/lsilveira/smartnews/scheduler/SchedulerTimeScale.kt
+++ src/main/kotlin/com/lsilveira/smartnews/scheduler/SchedulerTimeScale.kt
@@ -1,9 +1,9 @@
 package com.lsilveira.smartnews.scheduler
 
-enum class SchedulerTimeScale
+enum class SchedulerTimeScale(val scale: Int)
 {
-    SECONDS,
-    MINUTES,
-    HOURS,
-    DAYS
+    SECONDS(1000),
+    MINUTES(60*1000),
+    HOURS(60*60*1000),
+    DAYS(24*60*60*1000);
 }
